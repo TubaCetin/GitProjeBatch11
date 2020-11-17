4c7e5432cec957962e866bae397e32e5567d830f
TubaCetin <tubac0310@gmail.com>
2020-11-17 20:44:23 +0100

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -4,10 +4,10 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		
 		System.out.println("9:06-Birinci Gun");
-
 		System.out.println("İkinci Commit");
 		System.out.println("9:15-Ikinci Gun");
 		System.out.println("9:26-Ikinci Gun");
+		
 		
 	}
 
