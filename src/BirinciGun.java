9637c7373b1abbfd0797cdd546d885673cf0033e
TubaCetin <74511749+TubaCetin@users.noreply.github.com>
2020-11-17 21:24:58 +0100

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,14 +1,23 @@
 
 public class BirinciGun {
 	public static void main(String[] args) {
-		System.out.println("9:06-Birinci Gun");
-		System.out.println("İkinci Commit");
-		System.out.println("9:15-Ikinci Gun");
-		System.out.println("9:26-Ikinci Gun");
-		System.out.println("9:35 - ikinci gün");
-		System.out.println("10:05 - local");
-
-
+		System.out.println("11:57 - Birinci Gun");
+		System.out.println("ikinci Commit");
+		System.out.println("10:11 - ikinci Gun");
+		System.out.println("10:26 - ikinci gün");
+		System.out.println("10:35 - ikinci gün");
+		System.out.println("11:05 - local");
+		System.out.println("11:23 - github.com");
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
