db5c069065c5e3901a212fdf67c875092ee1abfe
TubaCetin <tubac0310@gmail.com>
2020-11-18 21:06:20 +0100

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -13,6 +13,7 @@ public class BirinciGun {
 		System.out.println("yepyeni bir satir");
 		System.out.println("8:26 feature branch");
 		System.out.println("task ");
+		System.out.println("task 2 ");
 
 
 	}
